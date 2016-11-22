92328f7eb10547bce6687d1d585ed3b1d1951dab
Fillype Farias <fillype1@hotmail.com>
2016-11-22 16:32:17 -0200

    create db seed and action methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,41 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Articles.all
+  end
+
+  def show
+    @article = Articles.find(params[:id])
+  end
+
+  def new
+    @article = Articles.new(params[:title], params[:content])
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to #index page
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to #index page
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to #index page
+  end
+
+  private
+
+  def article_params
+  end
 end
